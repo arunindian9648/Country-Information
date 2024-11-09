c362f50cc849328f132dc702928afbf71d0429a7
Arun Kumar Yadav <100112719+arunindian9648@users.noreply.github.com>
2024-11-09 23:04:16 +0530

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,63 +1,62 @@
-const countryInput=document.getElementById('countryInput');
-const search=document.getElementById('search');
+const countryInput = document.getElementById('countryInput');
+const search = document.getElementById('search');
 
-let search_result=document.getElementById('search_result');
-let flag=document.getElementById('flag');
-let name=document.getElementById('name');
-let capital=document.getElementById('capital');
-let continent=document.getElementById('continent');
-let currency=document.getElementById('currency');
-let sub_region=document.getElementById('sub_region');
-let coatOfArms=document.getElementById('coatOfArms');
-let language =document.getElementById('language');
-let cs=document.getElementById('cs');
-let tz=document.getElementById('tz');
-search.addEventListener('click',()=>{
+let search_result = document.getElementById('search_result');
+let flag = document.getElementById('flag');
+let name = document.getElementById('name');
+let capital = document.getElementById('capital');
+let continent = document.getElementById('continent');
+let currency = document.getElementById('currency');
+let sub_region = document.getElementById('sub_region');
+let coatOfArms = document.getElementById('coatOfArms');
+let language = document.getElementById('language');
+let cs = document.getElementById('cs');
+let tz = document.getElementById('tz');
 
-    let countryName=countryInput.value ;
-    let finalURL=`https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalURL);
-   
-    try{
-    fetch(finalURL)
- 
-   
-    .then((res)=>res.json())
-     .then((data)=>{
-        search_result.style.display='block';
+// The loading element
+let loading = document.getElementById('loading');
 
-        countryData=data[0];
+// Add event listener to the search button
+search.addEventListener('click', async () => {
+    let countryName = countryInput.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
-        console.log(countryData);
+    // Show the loading screen
+    loading.style.display = 'block';
+    search_result.style.display = 'none'; // Hide search result while loading
 
-        flag.src=countryData.flags.svg;
-        flag.alt=countryData.flags.alt;
-        coatOfArms.src=countryData.coatOfArms.svg;
-        coatOfArms.alt=countryData.coatOfArms.alt;
+    try {
+        // Fetch country data
+        let res = await fetch(finalURL);
+        let data = await res.json();
 
-  
-        name.innerHTML=countryData.name.official;
+        if (res.ok) {
+            search_result.style.display = 'block'; // Show search result once data is fetched
+            loading.style.display = 'none'; // Hide loading screen
 
-        capital.innerHTML=countryData.capital;
+            let countryData = data[0];
 
-        continent.innerHTML=countryData.continents;
+            flag.src = countryData.flags.svg;
+            flag.alt = countryData.flags.alt;
+            coatOfArms.src = countryData.coatOfArms.svg;
+            coatOfArms.alt = countryData.coatOfArms.alt;
 
-        sub_region.innerHTML=countryData.subregion;
+            name.innerHTML = countryData.name.official;
+            capital.innerHTML = countryData.capital;
+            continent.innerHTML = countryData.continents;
+            sub_region.innerHTML = countryData.subregion;
+            currency.innerHTML = countryData.currencies[Object.keys(countryData.currencies)].name;
+            cs.innerHTML = countryData.currencies[Object.keys(countryData.currencies)].symbol;
+            tz.innerHTML = countryData.timezones;
+            language.innerHTML = Object.values(countryData.languages).toString().split(',').join(', ');
 
-        currency.innerHTML=countryData.currencies[Object.keys(countryData.currencies)].name;
-
-       cs.innerHTML=countryData.currencies[Object.keys(countryData.currencies)].symbol;
-
-        tz.innerHTML=countryData.timezones;
-
-        language.innerHTML=Object.values(countryData.languages).toString().split(',').join(',');
-     });
+        } else {
+            loading.style.display = 'none'; // Hide loading screen on error
+            alert("Wrong input given");
+        }
+    } catch (e) {
+        loading.style.display = 'none'; // Hide loading screen if there is an error
+        console.log("Error: ", e);
+        alert("An error occurred while fetching the data.");
     }
-     catch(e){
-        console.log("Error: ",e);
-        
-    }
-
-    }); 
-
-
+});
